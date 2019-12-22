5283b3635ec1cc29d9c05c6e23f34a09783ef2cc
leoboyerbx <leoboyer.bx@gmail.com>
2019-12-22 09:57:59 +0100

    fixed database

--- server/classes/MysqlDb.js
+++ server/classes/MysqlDb.js
@@ -1,7 +1,9 @@
 const mysql = require('mysql')
+const config = require('../config/secret')
+
 class MySqlDb {
   static getConnection () {
-    const connection = mysql.createConnection()
+    const connection = mysql.createConnection(config)
     connection.connect()
     return connection
   }
