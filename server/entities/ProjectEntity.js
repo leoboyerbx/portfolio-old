90e32d3fedbee684f3daba6a4f25a52cd21c4ef4
leoboyerbx <leo.boyer@outlook.com>
2020-03-21 11:59:57 +0100

    Created getter for images in project entity

--- server/entities/ProjectEntity.js
+++ server/entities/ProjectEntity.js
@@ -19,6 +19,27 @@ class ProjectEntity {
     return '/user/projects/img/thumbnails/' + this.img_name
   }
 
+  generateImagesHtml (imagesString) {
+    const images = imagesString.split(', ').join(',').split(',')
+    let markup = `<div class="images">`
+    images.map(imagePath => {
+      if (imagePath === 'break') {
+        markup += `</div><div class="images">`
+      } else {
+        markup += `<a is="cursor-link" cursor-class="zoom" class="img-wrapper"><img is="lightbox-img" src="${imagePath}" alt="" class="img-project"></a>`
+      }
+    })
+    markup += '</div>'
+    return markup
+  }
+
+  get mediaHtml () {
+    if (this.media_type === 'images') {
+      return this.generateImagesHtml(this.media_content)
+    }
+    return ''
+  }
+
   get tags () {
     return this.raw_tags.split(', ').join(',').split(',')
   }
