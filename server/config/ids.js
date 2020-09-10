1a8120cc7e3b1b20b59018dadd4db8f33513b2b7
leoboyerbx <contact@leoboyer.fr>
2020-09-10 23:22:59 +0200

    Update gitlab-ci.yml

--- server/config/ids.js
+++ server/config/ids.js
@@ -2,7 +2,7 @@ function removeQuotes (str) {
     return str.split('"').join('')
 }
 module.exports = {
-    host: removeQuotes(process.env.MYSQL_HOST),
+    host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
